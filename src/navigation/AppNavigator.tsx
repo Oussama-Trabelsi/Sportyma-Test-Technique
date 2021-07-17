f74f566a4ca5d3d0473be32eead284ba67710c42
Oussama-Trabelsi <oussama.trabelsidev@gmail.com>
2021-07-17 04:13:52 +0200

    added splash screen to pre load all data

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,14 +2,16 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Root from 'src/screens/Root';
 import Home from 'src/screens/Home';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName="Root">
+        <Stack.Screen name="Root" component={Root} options={{headerShown:false}}/>
         <Stack.Screen name="Home" component={Home} options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/Root/index.tsx
+++ src/screens/Root/index.tsx
@@ -1,16 +1,43 @@
-// screens/Home.js
+import React, { useEffect, useState } from 'react';
+import { View, Image, Animated } from 'react-native';
+// Theme
+import styles from './style';
+import { Images } from 'src/theme';
 
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-
-class Root extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Root screen</Text>
-      </View>
-    );
-  }
+interface Props {
+  navigation: any;
 }
 
+const Root: React.FC<Props> = (props) => {
+  const [LogoAnime] = useState<Animated.Value>(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.spring(LogoAnime, {
+        toValue: 1,
+        tension: 10,
+        friction: 2,
+        useNativeDriver: false,
+      }),
+    ]).start(() => {
+      // todo : navigate after data loading instead of timeout
+      setTimeout(() => props.navigation.navigate('Home'), 1500);
+    });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={{
+          opacity: LogoAnime,
+          top: LogoAnime.interpolate({
+            inputRange: [0, 1],
+            outputRange: [80, 0],
+          }),
+        }}>
+        <Image source={Images.logo} />
+      </Animated.View>
+    </View>
+  );
+};
 export default Root;
